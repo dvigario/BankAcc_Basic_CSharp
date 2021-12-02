0e5263d3602382ddfea940a60807361c15ab6cd7
DViga <42376959+dvigario@users.noreply.github.com>
2021-12-02 17:16:55 +0000

    Creation of the BankAcc class
    Creation of the BankAcc class
    . set
    . Get
    . Constructor
    . Constructor Overload
    . ToString()
    . Placeholder

---
+++ BankAcc/BankAcc/BankAcc.cs
@@ -0,0 +1,35 @@
+﻿namespace BankAcc
+{
+    class BancAcc
+    {
+        public int AccNumber { get; private set; }
+        public string AccName { get; set; }
+        public double Balance { get; private set; }
+
+        public BancAcc(int accNumber, string accName)
+        {
+            AccNumber = accNumber;
+            AccName = accName;
+        }
+
+        public BancAcc(int accNumber, string accName, double balance) : this(accNumber, accName)
+        {
+            Balance = balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdrawal(double amount)
+        {
+            Balance -= amount + 2.0 // 2.0 = tax
+        }
+
+        public override string ToString()
+        {
+            return ($"Acc Number: {AccNumber}, Acc Name: {AccName}, Balance: £{Balance:F2}");
+        }
+    }
+}
